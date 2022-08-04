1f3c15232004fffe4a50429d31d088ec4e30a7bc
vzsoares <jogolots2@gmail.com>
2022-08-04 14:29:33 -0300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import "./App.css";
 import Profiler from "./components/Profiler";
 import Form from "./components/Form";
 
 function App() {
   return (
-    <div className='App'>
+    <>
       <Profiler />
       <Form />
-    </div>
+    </>
   );
 }
 
