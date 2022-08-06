7505e4933b014234c62cb07a81a71d6b3ad3258a
vzsoares <jogolots2@gmail.com>
2022-08-06 11:28:20 -0300

    enh:hooks types

--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -1,10 +1,13 @@
 import useSWR from "swr";
+import { User } from "../models/User";
 
 export default function useUsers() {
   const URL = "http://localhost:4000/api/users";
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  const { data, error } = useSWR(URL, fetcher, { refreshInterval: 1000 });
+  const { data, error } = useSWR<User[] | undefined>(URL, fetcher, {
+    refreshInterval: 1000,
+  });
 
   return {
     users: data,
